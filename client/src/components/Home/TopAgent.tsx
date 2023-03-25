7c533027872f9013c059ab9764bfdb52201d2149
Gopisakthivel <gopisakthivel@alphabsolutions.com>
2023-03-25 12:54:16 +0530

    first commit

---
+++ client/src/components/Home/TopAgent.tsx
@@ -0,0 +1,12 @@
+import React from  'react'
+
+const TopAgent=()=>{
+
+    return (
+        <>
+         <h3>Hello Home</h3>
+        </>
+    )
+}
+
+export default TopAgent;
---
+++ client/src/components/index.ts
@@ -0,0 +1,26 @@
+// common
+import Profile from "./Common/profile";
+import PropertyCard from "./Common/propertycard";
+import CustomButton from "./Common/customButton";
+
+// charts
+import PieChart from "./charts/PieChart";
+import PropertyReferrals from "./charts/PropertyReferrals";
+import TotalRevenue from "./charts/TotalRevenue";
+
+// agent
+import AgentCard from "./agent/agentCard";
+
+// home
+import TopAgent from "./Home/TopAgent";
+
+export {
+    Profile,
+    PropertyCard,
+    CustomButton,
+    PieChart,
+    PropertyReferrals,
+    TotalRevenue,
+    AgentCard,
+    TopAgent,
+};
